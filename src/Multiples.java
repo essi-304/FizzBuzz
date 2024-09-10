79d626e66b690527dfebaa1870d711f3c28eb4be
essi-304 <esther.siu@mail.utoronto.ca>
2024-09-09 20:47:14 -0400

    made Multiples.java

---
+++ src/Multiples.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int count = 0;
+        for ( int i = 1; i <= 1000; i++){
+            if (i % 3 == 0 || i % 5 ==0){
+                count++;
+            }
+        }
+        System.out.println("There are " + count + " multiple of 3 or 5 below 1000");
+    }
+}
